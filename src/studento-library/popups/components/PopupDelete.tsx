698acc876ba6076a24f2fe6d5f7e61a7b57b8d7c
Adam Pazderka <pazderka.adam@protonmail.com>
2022-02-24 20:27:35 +0100

    Add: PopupDelete.tsx

---
+++ src/studento-library/popups/components/PopupDelete.tsx
@@ -0,0 +1,37 @@
+import { FC, useState } from "react";
+import PopupBase from "../base-components/PopupBase";
+import { BtnDangerL } from '../../buttons/components/BtnDanger';
+import { BtnTertiaryL } from "../../buttons/components/BtnTertiary";
+import styled from "styled-components";
+
+interface IPopupDelete {
+    title: string;
+    children?: string;
+    event: Function;
+}
+
+const StyledButtons = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+    width: 100%;
+    justify-content: center;
+    gap: 16px;
+    margin-top: 24px;
+`
+
+const PopupDelete:FC<IPopupDelete> = (props) => {
+    const { title, children, event } = props;
+    const [ isOpened, setIsOpened ] = useState<boolean>(true);
+
+    return (
+        <PopupBase title={title} isOpened={isOpened} setIsOpened={setIsOpened}>
+            {children}
+            <StyledButtons>
+                <BtnTertiaryL onClick={() => setIsOpened(false)}>cancel</BtnTertiaryL>
+                <BtnDangerL icon={"delete"} onClick={() => {setIsOpened(false); event()}}>delete</BtnDangerL>
+            </StyledButtons>
+        </PopupBase>
+    )
+}
+
+export default PopupDelete;
