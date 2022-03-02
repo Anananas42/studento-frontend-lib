d708531d3c71227a1c1ad5bfd5b26a29b9424af8
Adam Pazderka <pazderka.adam@protonmail.com>
2022-03-02 15:59:27 +0100

    Add: Checkbox user action styling

--- src/studento-library/forms/shared/StyledCheckbox.tsx
+++ src/studento-library/forms/shared/StyledCheckbox.tsx
@@ -1,7 +1,20 @@
 import styled from "styled-components";
 
 const CheckboxColors = {
-    border: "hsl(34.28571428571429, 17.948717948717945%, 45%)",
+    bg1: "#FFBB3D",
+    bg2: "#FFD53D",
+    Default: {
+        innerShadow: "rgba(69, 60, 48, 0.06)",
+        border: "hsl(34.28571428571429, 17.948717948717945%, 50%)",
+    },
+    Hover: {
+        innerShadow: "hsla(38.96907216494845, 100%, 81.96078431372549%, 0.5)",
+        border: "hsl(34.28571428571429, 17.948717948717945%, 35%)",
+    },
+    Active: {
+        innerShadow: "hsla(38.96907216494845, 100%, 81.96078431372549%, 0.8)",
+        border: "hsl(34.28571428571429, 17.948717948717945%, 55%)",
+    }
 }
 
 interface IStyledCheckbox {
@@ -17,11 +30,22 @@ const StyledCheckbox = styled.input<IStyledCheckbox>`
     color: currentColor;
     width: 24px;
     height: 24px;
-    border: 2px solid ${CheckboxColors.border};
+    border: 2px solid ${CheckboxColors.Default.border};
     border-radius: 4px;
     transform: translateY(4px);
     display:inline-block;
     overflow: hidden;
+    box-shadow: inset 0 4px 8px ${CheckboxColors.Default.innerShadow};
+
+    :hover {
+        border-color: ${CheckboxColors.Hover.border};
+        box-shadow: inset 0 4px 16px ${CheckboxColors.Hover.innerShadow};
+    }
+
+    :active {
+        border-color: ${CheckboxColors.Active.border};
+        box-shadow: inset 0 4px 16px ${CheckboxColors.Active.innerShadow};
+    }
 
     &::before {
         content: "";
