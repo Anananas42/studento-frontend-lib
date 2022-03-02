03e0554d6a16eb2e46a4c84deedb4b27ccb24a58
Adam Pazderka <pazderka.adam@protonmail.com>
2022-03-02 18:24:56 +0100

    Add: Checkbox coloring

--- src/studento-library/forms/shared/StyledCheckbox.tsx
+++ src/studento-library/forms/shared/StyledCheckbox.tsx
@@ -1,19 +1,19 @@
 import styled from "styled-components";
 
 const CheckboxColors = {
-    bg1: "#FFBB3D",
-    bg2: "#FFD53D",
+    bg1: "#1C5FFF",
+    bg2: "#1C9EFF",
     Default: {
         innerShadow: "rgba(69, 60, 48, 0.06)",
-        border: "hsl(34.28571428571429, 17.948717948717945%, 50%)",
+        border: "hsl(33.749999999999986, 19.51219512195122%, 78%)",
     },
     Hover: {
         innerShadow: "hsla(38.96907216494845, 100%, 81.96078431372549%, 0.5)",
-        border: "hsl(34.28571428571429, 17.948717948717945%, 35%)",
+        border: "hsl(33.749999999999986, 19.51219512195122%, 53%)",
     },
     Active: {
         innerShadow: "hsla(38.96907216494845, 100%, 81.96078431372549%, 0.8)",
-        border: "hsl(34.28571428571429, 17.948717948717945%, 55%)",
+        border: "hsl(33.749999999999986, 19.51219512195122%, 64%)",
     }
 }
 
@@ -34,12 +34,17 @@ const StyledCheckbox = styled.input<IStyledCheckbox>`
     border-radius: 4px;
     transform: translateY(4px);
     display:inline-block;
+    position: relative;
     overflow: hidden;
     box-shadow: inset 0 4px 8px ${CheckboxColors.Default.innerShadow};
 
     :hover {
         border-color: ${CheckboxColors.Hover.border};
         box-shadow: inset 0 4px 16px ${CheckboxColors.Hover.innerShadow};
+
+        &:checked::before {
+            background: ${CheckboxColors.bg1};
+        }
     }
 
     :active {
@@ -49,15 +54,29 @@ const StyledCheckbox = styled.input<IStyledCheckbox>`
 
     &::before {
         content: "";
-        display:inline-block;
+        display: inline-block;
         width: 100%;
         height: 100%;
         transform: scale(0);
-        background-color: aqua;
+        background: linear-gradient(45deg, ${CheckboxColors.bg1}, ${CheckboxColors.bg2});
     }
 
     &:checked::before {
+        content: "";
         transform: scale(1);
+    }
+
+    &:checked::after {
+        content: "";
+        position: absolute;
+        display: inline-block;
+        width: 100%;
+        height: 100%;
+        top: -2px;
+        left: -2px;
+        clip-path: path('M9 16.17L5.53 12.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.18 4.18c.39.39 1.02.39 1.41 0L20.29 7.71c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L9 16.17z');
+        background: #fff;
+        z-index: 9000;
     }
 
 `;
