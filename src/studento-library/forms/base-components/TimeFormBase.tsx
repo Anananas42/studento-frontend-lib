5e7787509796a4ee6a36db4e37f714972492f30c
Adam Pazderka <pazderka.adam@protonmail.com>
2022-03-03 12:37:17 +0100

    Change: Make TimeFormBase use default input time (can be am/pm)

--- src/studento-library/forms/base-components/TimeFormBase.tsx
+++ src/studento-library/forms/base-components/TimeFormBase.tsx
@@ -15,7 +15,7 @@ export const StyledTimeForm = styled.input<IStyledInput>`
     font-size: 20px;
     padding: 8px ${props => props.borderRadius} ${props => props.icon ? "8px 54px" : ""};
     font-family: 'Varela Round', sans-serif;
-    width: 240px;
+    width: 200px;
 
     ::placeholder {
         color: ${props => props.fill};
@@ -66,78 +66,18 @@ const checkMinute = (time:string) => {
     return "";
 }
 
-// Need to solve this by creating invisible native input time and an overlapping custom visualization. When the visualization is clicked, the native input is focused and accepts values,
-// visualization is then updated accordingly.
-
 const TimeFormBase:FC<ITimeFormProps> = (props) => {
     const { value, setValue, label, formId, isDisabled, errorMessage, ...rest} = props;
     const { borderRadius, colors } = useThemeContext();
-    const [ hiddenInput, setHiddenInput ] = useState<string>("");
-    const [ shownInput, setShownInput ] = useState<string>("");
 
     const onKeyDown = () => {
 
     }
 
-    const processInput = (time: string) => {
+    const processInput = (time: any) => {
         console.log(time);
-        setShownInput(time);
+        setValue(time);
         return;
-
-        let newHiddenInput: string = time.replaceAll('-', '').replace(':', '');
-        console.log(newHiddenInput);
-
-        switch (newHiddenInput.length) {
-            case 0:
-                newHiddenInput = "";
-                break;
-            case 1:
-                const firstDigit = Number(newHiddenInput[0]);
-                if (firstDigit < 1 || firstDigit > 2) return;
-                newHiddenInput = newHiddenInput[0];
-                break;
-            case 2:
-                if (!checkHour(newHiddenInput)) return;
-                newHiddenInput = newHiddenInput.substring(0, 2);
-                break;
-            case 3:
-                if (hiddenInput.length === 4) {
-                    newHiddenInput = newHiddenInput.substring(0, 2);
-                    break;
-                }
-
-                if (!checkHour(newHiddenInput)) return; // Need to recheck due to copy paste
-                const thirdDigit = Number(newHiddenInput[0]);
-                if (thirdDigit < 0 || thirdDigit > 59) return;
-                newHiddenInput = newHiddenInput.substring(0, 3);
-                break;
-            case 4:
-                if (!checkHour(newHiddenInput) || !checkMinute(newHiddenInput)) return;
-                newHiddenInput = newHiddenInput;
-                break;
-            default:
-                return;
-
-        }
-        console.log(newHiddenInput);
-        setHiddenInput(newHiddenInput);
-        updateShownInput(newHiddenInput);
-    }
-
-    const updateShownInput = (hiddenInput: string) => {
-        let newShownInput:string = "--:--";
-        const len = hiddenInput.length;
-
-        if (len < 3) {
-            newShownInput = hiddenInput + newShownInput.substring(len);
-        }else if (len === 3) {
-            newShownInput = `${hiddenInput.substring(0, 2)}:${hiddenInput.substring(2)}-`;
-        }else if (len === 4) {
-            newShownInput = `${hiddenInput.substring(0, 2)}:${hiddenInput.substring(2)}`;
-        }
-
-        console.log(newShownInput);
-        setShownInput(newShownInput);
     }
 
     return (
@@ -146,8 +86,8 @@ const TimeFormBase:FC<ITimeFormProps> = (props) => {
                 <IconL>schedule</IconL>
             </StyledIconWrapper>
 
-            <StyledTimeForm type={"time"} step={60} min="0" max="2359" maxLength={6} id={formId ? formId : label} value={shownInput} onChange={(e) => processInput(e.target.value)} onKeyDown={onKeyDown} borderRadius={borderRadius}
-             fill={colors.fill} isError={errorMessage} disabled={isDisabled} placeholder={isDisabled ? "" : placeholder} width={"140px"} icon={"schedule"} />
+            <StyledTimeForm type={"time"} step={60} id={formId ? formId : label} value={value} onChange={(e) => processInput(e.target.value)} onKeyDown={onKeyDown} borderRadius={borderRadius}
+             fill={colors.fill} isError={errorMessage} disabled={isDisabled} placeholder={isDisabled ? "" : placeholder} width={"140px"} icon={"schedule"} required />
         </FormBase>
     );
 }
