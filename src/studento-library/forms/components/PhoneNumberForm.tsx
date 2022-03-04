e7480015ccc9a673ec178088c09c3a0f099441eb
Adam Pazderka <pazderka.adam@protonmail.com>
2022-03-04 18:48:57 +0100

    Add: PhoneNumberForm

---
+++ src/studento-library/forms/components/PhoneNumberForm.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+import { useThemeContext } from "../../ThemeProvider";
+import { TextFormBase } from "../base-components";
+
+interface IPhoneNumberForm {
+    value: string;
+    setValue: React.Dispatch<React.SetStateAction<string>>;
+    isHorizontal?: boolean; // Place label to the left
+    formId?: string; // To link label and input
+    errorMessage?: string; // Trigger error state by setting errorMessage to a value
+    isDisabled?: boolean;
+    isOptional?: boolean;
+    width?: string; // Set width manually. Else it's 100%
+    onKeyDown?: React.KeyboardEventHandler<any>; // Detect pressed keys while focused on the input (for example confirm something on enter)
+}
+
+const PhoneNumberForm:FC<IPhoneNumberForm> = (props) => {
+    const { languageMap } = useThemeContext();
+
+    return (
+        <TextFormBase icon={"phone"} label={languageMap.Contact.phoneNumber} placeholder={""} {...props} />
+    )
+}
+
+export default PhoneNumberForm;
