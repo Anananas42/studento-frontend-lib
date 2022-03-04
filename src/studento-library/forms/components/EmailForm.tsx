401647344f407480439a1e89e5395762e3e2a513
Adam Pazderka <pazderka.adam@protonmail.com>
2022-03-04 18:49:03 +0100

    Add: EmailForm

---
+++ src/studento-library/forms/components/EmailForm.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+import { useThemeContext } from "../../ThemeProvider";
+import { TextFormBase } from "../base-components";
+
+interface IEmailForm {
+    value: string;
+    setValue: React.Dispatch<React.SetStateAction<string>>;
+    isHorizontal?: boolean; // Place label to the left
+    formId?: string; // To link label and input
+    errorMessage?: string; // Trigger error state by setting errorMessage to a value
+    isDisabled?: boolean;
+    isOptional?: boolean;
+    width?: string; // Set width manually. Else it's 100%
+    onKeyDown?: React.KeyboardEventHandler<any>; // Detect pressed keys while focused on the input (for example confirm something on enter)
+}
+
+const EmailForm:FC<IEmailForm> = (props) => {
+    const { languageMap } = useThemeContext();
+
+    return (
+        <TextFormBase icon={"email"} label={languageMap.Contact.email} placeholder={""} {...props} />
+    )
+}
+
+export default EmailForm;
