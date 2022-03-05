19d7b03871e5e11f477538ef84783d2c3d22c8b2
Adam Pazderka <pazderka.adam@protonmail.com>
2022-03-06 00:07:57 +0100

    Add: NavLoginBtn

---
+++ src/studento-library/navigation/base-components/NavLoginBtn.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+import styled from "styled-components";
+import { BtnTextL } from "../../buttons/components/BtnText";
+import { useThemeContext } from "../../ThemeProvider";
+
+interface IStyledNavLoginBtn {
+    bg: string;
+    fill: string;
+    borderRadius: string;
+}
+
+const StyledNavLoginBtn = styled.div<IStyledNavLoginBtn>`
+    display: flex;
+    align-items: center;
+    transform: translateX(50%);
+    user-select: none;
+    color: ${props => props.fill};
+    background-color: ${props => props.bg};
+    border-radius: 0 0 16px 16px;
+    padding: 0px 16px;
+`;
+
+const NavLoginBtn:FC = () => {
+    const { colors, borderRadius, languageMap } = useThemeContext();
+
+    return (
+        <StyledNavLoginBtn bg={colors.primary} fill={colors.fill} borderRadius={borderRadius}>
+            <BtnTextL icon={"login"} isAfter={true} onClick={() => 0}>{languageMap.Generic.login}</BtnTextL>
+        </StyledNavLoginBtn>
+    )
+}
+
+export default NavLoginBtn;
