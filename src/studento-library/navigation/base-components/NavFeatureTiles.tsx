4132e399c574ad7a9cd85bd37b34cff5f92b282b
Adam Pazderka <pazderka.adam@protonmail.com>
2022-03-07 23:16:07 +0100

    Update: NavFeatureTile layout

--- src/studento-library/navigation/base-components/NavFeatureTiles.tsx
+++ src/studento-library/navigation/base-components/NavFeatureTiles.tsx
@@ -25,7 +25,7 @@ const StyledDropdown = styled.div<StyleProps>`
 
     :hover > :last-child {
         display: grid;
-        grid-template-rows: repeat(3, 1fr);
+        grid-template-rows: repeat(4, 1fr);
         grid-auto-flow: column;
     }
 `;
