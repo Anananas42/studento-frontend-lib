8b42c4795060d7853f17084c6040f53942bdb4a8
Adam Pazderka <pazderka.adam@protonmail.com>
2022-03-09 18:20:14 +0100

    Add: index to button components for more organized importing

---
+++ src/component-library/buttons/components/index.tsx
@@ -0,0 +1,12 @@
+import { BtnCloseS, BtnCloseM, BtnCloseL } from "./BtnClose";
+import { BtnConfirmS, BtnConfirmM, BtnConfirmL } from "./BtnConfirm";
+import { BtnCTAS, BtnCTAM, BtnCTAL } from "./BtnCTA";
+import { BtnDangerS, BtnDangerM, BtnDangerL } from "./BtnDanger";
+import { BtnOutlineS, BtnOutlineM, BtnOutlineL } from "./BtnOutline";
+import { BtnPrimaryS, BtnPrimaryM, BtnPrimaryL } from "./BtnPrimary";
+import { BtnSecondaryS, BtnSecondaryM, BtnSecondaryL } from "./BtnSecondary";
+import { BtnTertiaryS, BtnTertiaryM, BtnTertiaryL } from "./BtnTertiary";
+import { BtnTextS, BtnTextM, BtnTextL } from "./BtnText";
+
+export default {BtnCloseS, BtnCloseM, BtnCloseL, BtnConfirmS, BtnConfirmM, BtnConfirmL, BtnCTAS, BtnCTAM, BtnCTAL, BtnDangerS, BtnDangerM, BtnDangerL, BtnOutlineS, BtnOutlineM, BtnOutlineL, 
+    BtnPrimaryS, BtnPrimaryM, BtnPrimaryL, BtnSecondaryS, BtnSecondaryM, BtnSecondaryL, BtnTertiaryS, BtnTertiaryM, BtnTertiaryL, BtnTextS, BtnTextM, BtnTextL};
