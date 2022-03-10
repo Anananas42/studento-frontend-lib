18660639f0cbb74739df6dc5902fddc679bff8de
Adam Pazderka <pazderka.adam@protonmail.com>
2022-03-10 13:28:21 +0100

    Add: public section for pages available to unlogged users

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -2,8 +2,8 @@ import { FC, useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import DefaultNavbar from "../component-library/navigation/components/DefaultNavbar";
 import { useUserContext } from "../component-library/UserProvider";
-import Home from "../pages/shared/Home";
-import Login from "../pages/shared/Login";
+import Home from "../pages/public/Home";
+import Login from "../pages/public/Login";
 
 const PublicRoutes:FC = () => {
     const navigate = useNavigate();
